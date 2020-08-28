7829f1fd629c164e8e891faf11c59b56d129dee3
yilianwu <itsgoogl3r@gmail.com>
2020-08-28 17:49:03 +0800

    add ctrl file

---
+++ reincubate_ctrl/reincubate_ctrl.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>
+Servo myservo[3];
+float m_len[3];
+float m_theta[3];
+float theta; //培養皿傾斜夾角
+float theta_max;
+float r1=250; //培養皿半徑(mm)
+float r2;
+float delta_m=33;
+float delta_p;
+float count=0;
+float t;
+float spd=0.5;
+void setup() {
+  Serial.begin(9600);
+  
+  delta_p=(4/3)*delta_m;
+  theta_max=asin(delta_p/(r1*2));
+  theta=theta_max;
+  r2=r1*cos(theta);
+
+  myservo[0].attach(9);
+  myservo[1].attach(10);
+  myservo[2].attach(11);
+
+  for(int i=0;i<3;i++){
+    myservo[i].write(0); //reset
+  }
+}
+
+void loop() {
+  count++;
+  t=radians(count*spd);
+  for(int i=0;i<3;i++){
+  m_len[i]=tan(theta)*(abs((cos(t)/sin(t))*r2*cos(TWO_PI*i/3)+r2*sin(TWO_PI*i/3)-(r2/sin(t)))/sqrt(sq(cos(t)/sin(t))+1));
+  m_theta[i]=mapf(m_len[i],0,delta_m,0,180);
+  myservo[i].write(m_theta[i]);
+  }
+  
+  delay(30);
+
+}
+
+//map float number
+float mapf(float x, float fromLow, float fromHigh, float toLow, float toHigh)
+{
+  return (x - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+}
